f142885824725565a9c6332d708f23ab3b360876
yugan2005X230Win <xiongzhehong@gmail.com>
2015-08-17 09:50:57 -0700

    Add Junit test folder

---
+++ UdemyOOD/test/edu/udemy/ood/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package edu.udemy.ood;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
